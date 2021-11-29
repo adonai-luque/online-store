cb4c42fa75285b90015b6c2cb4ce0965def6a3b9
adonai-luque <akluqueprieto@gmail.com>
2021-11-28 23:15:29 -0500

    Added fetchCategories method and refactor fetchProducts with clear search functionality

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-async function fetchProducts(query = '') {
+function fetchProducts(query = '') {
   let url = (query === '') ? 'http://127.0.0.1:3000/products' : `http://127.0.0.1:3000/products/?query=${query}`
   let headers = new Headers();
   headers.append('Content-Type', 'application/json');
-  const response = await fetch(url, {header: headers});
-  return response.json()
+  let products = fetch(url, {header: headers})
+    .then(response => response.json())
+  return products
 }
 
-function renderProducts(products) {
+function fetchCategories() {
+  let url = 'http://127.0.0.1:3000/categories'
+  let headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  let categories = fetch(url, {header: headers})
+    .then(response => response.json())
+  return categories
+}
+
+const renderProducts = (products) => {
+  
+  let content = document.getElementById('content')
+  content.innerHTML = ""
+
+  if (products.length === 0) content.innerHTML = "Sin resultados"
+  
   const card = (name, url_image='./assets/no-image.jpg', price) => {
     if ((url_image === "") || (url_image === null)) {
       url_image = './assets/no-image.jpg'
@@ -18,26 +34,38 @@ function renderProducts(products) {
       <img src="${url_image}" class="card-img-top mt-2 mb-4" alt="${name}">
       <div class="d-flex flex-column justify-content-around align-items-center m-4">
         <h5>${name}</h5>
-        <p>${price}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <p>$ ${price}</p>
+        <a href="#" class="btn btn-primary">Agregar al carrito</a>
       </div>
     `;
     return element;
   }
   
-  let content = document.getElementById('content')
-  content.innerHTML = ""
+  
   products.forEach(product => {
     content.append(card(product.name, product.url_image, product.price))
   });
 }
 
-let queryInput = document.getElementById('query')
-let searchButton = document.querySelector('form.d-flex')
-searchButton.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let query = queryInput.value
-  if (query) fetchProducts(queryInput.value).then(products => renderProducts(products))
+function addEventListeners() {
+  let queryInput = document.getElementById('query-input')
+  let searchForm = document.getElementById('search-form')
+  searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let query = queryInput.value
+    if (query) fetchProducts(queryInput.value).then(products => renderProducts(products))
+  })
+  
+  queryInput.addEventListener('input', () => {
+    let query = queryInput.value
+    if (query === "") fetchProducts().then(products => renderProducts(products))
+  })
+}
+
+
+Promise.all([fetchProducts(), fetchCategories()]).then(([products, categories]) => {
+  addEventListeners()
+  renderProducts(products)
+  console.log(categories)
 })
 
-fetchProducts().then(products => renderProducts(products))
